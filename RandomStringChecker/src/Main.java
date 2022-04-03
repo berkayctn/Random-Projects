39fd97a3b2ebf63d4bfe513a34989d3fa863399c
Berkay Cetin <berkaycetin1710@gmail.com>
2022-04-03 11:59:05 +0300

    Done !

---
+++ RandomStringChecker/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    //This code piece counts the characters of a randomly generated String expression.
+
+    public static void uniqueOrNot(String s) {
+        String result = "";
+        int countOfChar = 0;
+
+        ArrayList<Character> arrForStrings = new ArrayList<>();
+        HashMap<Character, Integer> timesOccurred = new HashMap<>();
+
+        //I filled the characters into an arraylist
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            arrForStrings.add(c);
+        }
+
+        //Check whether the given String is unique
+        for (int j = 0; j < arrForStrings.size(); j++) {
+            countOfChar = 0;
+
+            for (int y = 0; y < arrForStrings.size(); y++) {
+                if (arrForStrings.get(j) == arrForStrings.get(y)) {
+                    countOfChar++;
+                }
+
+                timesOccurred.put(arrForStrings.get(j), countOfChar);
+            }
+        }
+
+        System.out.println(timesOccurred);
+    }
+
+
+    public static String randomStringGenerator() {
+
+        //This code piece generates a 50 digits random string using ASCII codes
+        //Each time, the string is generated completely randomly
+
+        char[] arr = new char[50];
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            double randomNumber = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
+            char convertedChar = (char) randomNumber;
+            arr[i] = convertedChar;
+            s.append(arr[i]);
+        }
+        return s.toString();
+    }
+
+
+    public static void main(String[] args) {
+        uniqueOrNot(randomStringGenerator());
+        uniqueOrNot(randomStringGenerator());
+        uniqueOrNot(randomStringGenerator());
+        uniqueOrNot(randomStringGenerator());
+        uniqueOrNot(randomStringGenerator());
+    }
+}
